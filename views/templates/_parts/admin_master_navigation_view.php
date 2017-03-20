b38ba0221f8c00b4be09090e3476791df4c0e404
3riik <papson.erik@gmail.com>
2017-03-20 18:43:51 +0100

    navigation

---
+++ views/templates/_parts/admin_master_navigation_view.php
@@ -0,0 +1,27 @@
+﻿
+
+<ul class="nav nav-tabs">
+ <li role="presentation" class="<?= empty(uri_string()) ? 'active' : ''?>">
+	<a href="<?= site_url()?>">Home</a>
+ </li>
+ <li role="presentation" class="dropdown <?= (strcmp(uri_string(),'pictures') >= 0) ? 'active' : ''?>">
+		<a class="dropdown-toggle " href="<?= site_url('pictures/')?>" data-toggle="dropdown"role="button" aria-haspopup="false" aria-expanded="false">
+		Obrázky <span class="caret"></span>
+		</a>
+		     <ul class="dropdown-menu">
+				<li class="<?= (strcmp(uri_string(),'pictures') == 0) ? 'active' : ''?>">
+					<a href="<?= site_url('pictures/')?>">Všetky obrázky</a>
+				</li>
+				<li role="separator" class="divider"></li>
+				<?php 
+				foreach ($categories as $category):
+				$uri = 'pictures/category/'.$category->name;
+				?>
+				<li <?= substr_compare($uri, uri_string(), 0, strlen($uri)) == 0 ? 'class="active"' : '' ?>>
+					<a href="<?=site_url($uri)?>"><?= $category->description?></a>
+				</li>
+				<?php    
+				endforeach;
+				?>
+			 </ul>
+ </li>
