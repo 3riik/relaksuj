595e632de1cdc1f1375451c4f2882f99a4be6d19
3riik <papson.erik@gmail.com>
2017-03-21 11:20:00 +0100

    one picture gif repair

--- views/pictures/show_picture_view.php
+++ views/pictures/show_picture_view.php
@@ -9,19 +9,34 @@ $author = $this->ion_auth->user($picture->uid)->row();
 
 <p class="description"><?= $picture->description?></p>
     <span class="time">Pridal: <?= $author->username?> | <?= date("d-m-Y | H:i",strtotime($picture->time)); ?></span>
+
+<?php if ($picture->file_ext == '.gif'){ ?>
 <div id="gif" class="picture" > 
+    <img class="img-rounded gif" data-state="static" src="<?php echo base_url(),'uploads/img/'.$picture->filename.'.jpg'?>"/>
+	<span data-state="static"  class="gif-txt">GIF</span>             
+</div>
+<script type="text/javascript">
+  $("#gif").click(function () {    		  
+		  var img = $(this).find('img');
+		  var gifTxt = $(this).find('span');
+	    if (img.attr("data-state") == "static") {
+	      $(img).attr("src", "<?= base_url().'./uploads/img/'.$picture->filename.'.gif'?>");
+	      $(img).attr("data-state", "dynamic");
+	      $(img).attr("id","<?= $picture->filename?>");
+	      $(gifTxt).attr("class","gif-none");
+	      $(gifTxt).text('');
+	    } else {
+	      $(img).attr("src", "<?= base_url().'./uploads/img/'.$picture->filename.'.jpg'?>");
+	      $(img).attr("data-state", "static");
+	      $(gifTxt).attr("class","gif-txt");
+	      $(gifTxt).text('GIF');
+	    }
+	  });
+	</script>
+<?php } else { ?>
+
+<div class="picture" > 
     <img class="img-rounded" data-state="static" src="<?php echo base_url(),'uploads/img/'.$picture->filename.'.jpg'?>"/>
 </div>
-<?php if ($picture->file_ext == '.gif'){ ?>
-<script type="text/javascript">
-  $("#gif").click(function () {    
-    if ($(this).find("img").attr("data-state") == "static") {
-      $(this).find("img").attr("src", "<?= base_url().'./uploads/img/'.$picture->filename.'.gif'?>");
-      $(this).find("img").attr("data-state", "dynamic");
-    } else {
-      $(this).find("img").attr("src", "<?= base_url().'./uploads/img/'.$picture->filename.'.jpg'?>");
-      $(this).find("img").attr("data-state", "static");
-    }
-  });
-</script>
-<?php }
+
+<?php }?>
